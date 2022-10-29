e029b517d8c2235f2040a5faf19313806ccc71c8
k2supra <andrijkorolevic@gmail.com>
2022-10-29 14:27:57 +0300

    Created new lab dev feature/hw_ex_1

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("bgdbfdbdb")
+def paloo(tt):
+    for pa in pal:
+        res = pal[::-1]
+    print(res)
+    if pal == res:
+        print("True")
+    else:
+        print("NOPE")
+
+
+try:
+    print("Введіть паліндром")
+    pal = input("->")
+
+
+
+
+
+
+    def main():
+        paloo(pal)
+
+
+    main()
+
+
+
+except Exception as ex:
+    print(f"Error", {ex})
