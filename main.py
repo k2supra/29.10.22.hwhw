b93952216edaf643f4c9ecb46540107d38e5354e
k2supra <andrijkorolevic@gmail.com>
2022-10-29 14:59:20 +0300

    Created new lab dev feature/hw_ex_3

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("bgdbfdbdb")
+def hg(t):
+    res = text.count(".") + text.count("?") + text.count("/") + text.count(",") + text.count(" ") + text.count(
+        "-") + text.count("_")
+    print(res)
+
+
+try:
+    print("Enter text")
+    text = input("->")
+
+
+    def main():
+        hg(text)
+
+    main()
+
+
+except Exception as ex:
+    print(f"Error", {ex})
